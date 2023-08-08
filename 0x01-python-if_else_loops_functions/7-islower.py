0bbbec6888cda93a03ab5cd1550252480a805390
Abdullah-Faik <abdullahfaik80@gmail.com>
2023-08-09 01:34:10 +0300

    task number 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import os
 def islower(c):
-    if c == "":
-        os.stderr.write("Traceback (most recent call last")
     if c >= 'a' and c <= 'z':
         return True
     return False
+
