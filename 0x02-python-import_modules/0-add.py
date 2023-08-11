fecaeb89500bc7abcb8b26f40a040438950c0b24
Abdullah-Faik <abdullahfaik80@gmail.com>
2023-08-11 08:39:11 +0300

    task number 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""This module contains a function that adds two integers"""
-from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
