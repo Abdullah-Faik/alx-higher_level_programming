36beb817d5ae1d4063c6a06684cccd58813df9af
Abdullah-Faik <abdullahfaik80@gmail.com>
2023-08-13 01:52:50 +0300

    task number 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    ls = my_list
+    ls = []
     if idx < 0 or idx >= len(my_list):
         return my_list
     else :
-        ls[idx] = element
+        for i in range(len(my_list)):
+            if i == idx :
+                ls.append(element)
+            else:
+                ls.append(my_list[i])
     return ls
