ce213c8f60bf2ffbb87a369d3dcf07e017a4c291
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-17 12:20:52 +0300

    adding

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,7 +4,3 @@ def print_sorted_dictionary(a_dictionary):
     a.sort()
     for x in a:
         print(x[0],": ",x[1],sep="")
-
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
