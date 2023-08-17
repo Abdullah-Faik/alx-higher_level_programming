3eb911fccaa1bf5ad25632aa3c20b6a448def4c4
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-17 12:21:19 +0300

    adding

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     a = list(a_dictionary.items())
     a.sort()
     for x in a:
-        print(x[0],": ",x[1],sep="")
+        print(x[0], ": ", x[1], sep="")
