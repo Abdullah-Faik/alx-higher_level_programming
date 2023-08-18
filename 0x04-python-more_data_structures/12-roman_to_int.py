0735e3363e4e39b610e5178a58ad117dfbc6d347
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-18 15:02:02 +0300

    adding

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-
-
-
-
-
-
-
-
-
-
+def roman_to_int(roman_string):
+    rm = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000}
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    else:
+        sum = 0
+        for i in range(len(roman_string)):
+            if i > 0 and rm[roman_string[i]] > rm[roman_string[i - 1]]:
+                sum += rm[roman_string[i]] - 2 * rm[roman_string[i - 1]]
+            else:
+                sum += rm[roman_string[i]]
+        return sum
 
 
 #roman number is represented by seven different symbols: I, V, X, L, C, D and M.
