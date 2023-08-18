f65a2c34de527aa54e14ba29e6e62e0850f47242
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-18 04:14:13 +0300

    adding

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key not in a_dictionary:
-        return a_dictionary
     for key in list(a_dictionary.keys()):
         if key == key:
             del a_dictionary[key]
