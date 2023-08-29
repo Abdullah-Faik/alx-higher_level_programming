971dc21b993b6cff967b2d23105067769e4dceb3
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-29 21:49:19 +0300

    task num 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        """__init__ method.
+        Args:
+            size (int): size of the square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """area method.
+        Returns:
+            the area of the square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """size getter.
+        Returns:
+            the size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter.
+        Args:
+            value (int): size of the square.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """my_print method.
+        Returns:
+            square with the character
+        """
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
