32b59ebb5437e49ab58c27a941452227d5a073a3
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-29 21:38:47 +0300

    fix 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,30 +10,9 @@ class Square:
         Args:
             size (int): size of the square.
         """
-        self.__size = size
-
-    @property
-    def size(self):
-        """size getter.
-        Returns:
-            size of the square.
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """size setter.
-        Args:
-            value (int): size value to be setted.
-        Raises:
-            TypeError: if size is not an integer
-            ValueError: if size is less than 0
-        Returns:
-            None.
-        """
-        if type(value) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >=0")
+        elif size < 0:
+            raise ValueError("ize must be >= 0")
         else:
-            self.__size = value
+            self.__size = size
