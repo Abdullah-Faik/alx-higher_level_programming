a819a5635fdfd24b30063a9cf8cafe711d92f6b5
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-29 20:45:16 +0300

    task _2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        """__init__ method.
+        Args:
+            size (int): size of the square.
+        """
+        self.__size = size
