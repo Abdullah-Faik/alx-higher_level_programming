f183b8b29347768810a5bddf4ed1aab0067b428c
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-30 01:05:24 +0300

    task 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -50,3 +50,42 @@ class Node:
             raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
+
+class SinglyLinkedList:
+    """SinglyLinkedList class"""
+
+    def __init__(self):
+        """__init__ method.
+        """
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """sorted_insert method.
+        Args:
+            value (int): data of the node.
+        """
+        new_node = Node(value)
+        if self.__head is None:
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            while current.next_node is not None and \
+                    current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        """__str__ method.
+        Returns:
+            string representation of the list.
+        """
+        string = ""
+        current = self.__head
+        while current is not None:
+            string += str(current.data) + "\n"
+            current = current.next_node
+        return string[:-1]
