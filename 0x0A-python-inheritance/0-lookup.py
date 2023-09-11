a15285250310d3f3ca2843e8ef4716960d1acf06
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-11 15:51:26 +0300

    fix betty

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,5 +3,5 @@
 
 
 def lookup(obj):
-    """Function that returns the list of available attributes and methods of an object"""
+    """return the list of available attributes and methods of an object"""
     return dir(obj)
