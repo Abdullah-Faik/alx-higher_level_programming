017408bd11b5268dd2ddefb6b77f4db846c818d8
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-12 18:51:10 +0300

    task num4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,5 +6,11 @@ import json
 
 
 def from_json_string(my_str):
+    """convert json to obj
+    Args:
+        my_str: recive obj
+        jsf: recive json file
+    Return:  obj file jsf
+    """
     jsf = json.loads(my_str)
     return jsf
