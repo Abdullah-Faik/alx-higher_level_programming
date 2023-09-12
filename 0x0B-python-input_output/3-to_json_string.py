554644ff7f1c473c9a04c86e96b954dfea57a7fc
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-12 18:43:07 +0300

    fix documentation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import json
 """convert object to json"""
+
+
+import json
 
 
 def to_json_string(my_obj):
     """convert objcts to json
     Args:
         my_obj: recive obj
-    Return:
-        jsf
+        jsf: recive json file
+    Return: jsf file json
     """
     jsf = json.dumps(my_obj)
     return jsf
