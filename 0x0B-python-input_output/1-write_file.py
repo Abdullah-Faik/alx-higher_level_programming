65fb97d5500c179ff32700035ec42c9ed2e93546
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-12 17:31:43 +0300

    tas num 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""write text in file"""
+
+
+def write_file(filename="", text=""):
+    """ write to file 
+    Args:
+        filename: file name
+        text: text to write
+    Return: number of characters written
+    """
+    with open(file=filename, mode="w", encoding="utf-8") as f:
+        written = f.write(text)
+        f.close()
+    return written
