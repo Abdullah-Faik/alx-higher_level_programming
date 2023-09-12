392391836303615879e75253dd127477e50a91a1
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-12 19:01:49 +0300

    fix some errors

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,9 +8,8 @@ import json
 def save_to_json_file(my_obj, filename):
     """ save object in fie as json """
 
-    js = json.dumps(my_obj)
     with open(file=filename, mode="w", encoding="utf-8") as f:
+        js = json.dumps(my_obj)
         pyts = f.write(js)
         f.close()
     return pyts
-
