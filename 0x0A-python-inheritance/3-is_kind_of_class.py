18e98258bfd0ae72f53f125aa24167f52fb560d8
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-13 15:49:36 +0300

    task num 4

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""check if object is istance from class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if ob ject in class"""
+    if isinstance(obj, a_class):
+        return True
+    return False
