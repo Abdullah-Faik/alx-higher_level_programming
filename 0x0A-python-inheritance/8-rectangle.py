730bcb3a31ad923a9b5079858f60021c790b6897
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-13 17:17:25 +0300

    fix betty

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,6 +5,8 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """ rectangle task"""
+
     def __init__(self, width, height):
         if super().integer_validator("width", width):
             self.width = width
