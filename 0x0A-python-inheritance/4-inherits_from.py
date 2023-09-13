6bcde87597e7652c9568530cfd04898a3c003b59
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-13 15:54:35 +0300

    tak nuum 5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """check if object is istance from class"""
-    if isinstance(obj, a_class.__bases__):
+    if isinstance(obj, a_class):
         return True
     return False
