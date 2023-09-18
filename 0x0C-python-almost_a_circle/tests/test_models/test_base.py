d9671fda4cc09fe0954385669c346526914cbb13
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-19 01:50:29 +0300

    tasks

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -76,101 +76,99 @@ class BaseTest(unittest.TestCase):
         self.assertEqual(Base.to_json_string(input), expected)
 
     def test_to_json_string(self):
-        """
-        Test to_json_string
-        """
-        jstr = Base.to_json_string(
-            [{'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}]
-        )
+        """Test to_json_string method."""
+        rect_list = [{'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}]
+
+        # Test valid input
+        jstr = Rectangle.to_json_string(rect_list)
         self.assertEqual(
-            jstr, '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]'
-        )
-        jstr = Base.to_json_string(None)
+            jstr, '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
+
+        # Test None input
+        jstr = Rectangle.to_json_string(None)
         self.assertEqual(jstr, "[]")
-        jstr = Base.to_json_string([])
+
+        # Test empty list input
+        jstr = Rectangle.to_json_string([])
         self.assertEqual(jstr, "[]")
 
     def test_from_json_string(self):
-        """ Test from_json_string """
-        list_input = [
-            {'id': 89, 'width': 10, 'height': 4},
-            {'id': 7, 'width': 1, 'height': 7}
-        ]
-        json_list_input = Base.to_json_string(list_input)
-        list_output = Base.from_json_string(json_list_input)
-        self.assertEqual(list_output, list_input)
-        self.assertEqual(Base.from_json_string(None), [])
-        self.assertEqual(Base.from_json_string(''), [])
+        """Test from_json_string method."""
+        rect_list = [{'id': 89, 'width': 10, 'height': 4},
+                     {'id': 7, 'width': 1, 'height': 7}]
+        json_list_input = Rectangle.to_json_string(rect_list)
+
+        # Test valid input
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list_output, rect_list)
+
+        # Test None input
+        self.assertEqual(Rectangle.from_json_string(None), [])
+
+        # Test empty string input
+        self.assertEqual(Rectangle.from_json_string(''), [])
 
     def test_create(self):
-        """ Test create """
+        """Test create method."""
         r1 = Rectangle(3, 5, 1, 1, 1)
         r1_dictionary = r1.to_dictionary()
         r2 = Rectangle.create(**r1_dictionary)
+
+        # Test object creation and comparison
         self.assertNotEqual(r1, r2)
         self.assertEqual(r1.to_dictionary(), r2.to_dictionary())
 
-    def test_2_create(self):
-        """ Test create """
-        dict_input = {'id': 89, 'width': 10, 'height': 4}
-        r1 = Rectangle.create(**dict_input)
-        self.assertNotEqual(r1.to_dictionary(), dict_input)
-        dict_out = {'id': 89, 'width': 10, 'height': 4, 'x': 0, 'y': 0}
-        self.assertEqual(r1.to_dictionary(), dict_out)
-        r2 = Rectangle(10, 4, 0, 0, 89)
-        self.assertEqual(r1.to_dictionary(), r2.to_dictionary())
-
     def test_load_from_file(self):
-        """ Test load_from_file """
+        """Test load_from_file method."""
         r1 = Rectangle(10, 7, 2, 8, 1)
         r2 = Rectangle(2, 4, 0, 0, 2)
         Rectangle.save_to_file([r1, r2])
+
+        # Test loading from file
         list_rectangles_output = Rectangle.load_from_file()
         self.assertNotEqual(list_rectangles_output, [r1, r2])
         self.assertEqual(
             [i.to_dictionary() for i in list_rectangles_output],
             [r1.to_dictionary(), r2.to_dictionary()]
         )
-        Rectangle.save_to_file(None)
-        list_rectangles_output = Rectangle.load_from_file()
-        self.assertEqual(list_rectangles_output, [])
-        Rectangle.save_to_file([])
-        list_rectangles_output = Rectangle.load_from_file()
-        self.assertEqual(list_rectangles_output, [])
 
     def test_load_from_file_not_exist(self):
-        """ Test load_from_file """
+        """Test load_from_file method when file doesn't exist."""
         try:
             os.remove("Rectangle.json")
         except:
             pass
+
+        # Test loading from non-existing file
         self.assertEqual(Rectangle.load_from_file(), [])
 
-    def test_load_from_file_1(self):
-        """ Test load_from_file """
+    def test_load_from_file_empty(self):
+        """Test load_from_file method with an empty file."""
         try:
             os.remove("Rectangle.json")
         except:
             pass
+
+        # Test loading from an empty file
         self.assertEqual(Rectangle.load_from_file(), [])
         self.assertEqual(Square.load_from_file(), [])
 
-    def test_0_save_to_file(self):
-        """ Test save_to_file """
+    def test_save_to_file(self):
+        """Test save_to_file method."""
         Rectangle.save_to_file(None)
         with open("Rectangle.json", "r") as f:
             self.assertEqual(f.read(), "[]")
+
         Rectangle.save_to_file([])
         with open("Rectangle.json", "r") as f:
             self.assertEqual(f.read(), "[]")
 
-    def test_1_save_to_file(self):
-        """ Test save_to_file """
-        r1 = Rectangle(10, 7, 2, 8, 1)
-        r2 = Rectangle(2, 4, 0, 0, 2)
-        Rectangle.save_to_file([r1, r2])
-        with open("Rectangle.json", "r") as f:
-            self.assertEqual(
-                json.loads(f.read()),
-                [r1.to_dictionary(), r2.to_dictionary()]
-            )
+    def test_str_for_default(self):
+        """Test __str__ method for default case."""
+        r1 = Rectangle(4, 6)
+        self.assertEqual(str(r1), "[Rectangle] (1) 0/0 - 4/6")
+
+    def test_str_for_full_rectangle(self):
+        """Test __str__ method for a full rectangle."""
+        r1 = Rectangle(5, 5)
+        self.assertEqual(str(r1), "[Rectangle] (1) 0/0 - 5/5")
