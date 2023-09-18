608141b6f7ebc97226a075f59bfb7b330005d289
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-18 20:46:10 +0300

    fix some error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,12 +20,14 @@ class Base:
         """reset nb_objects"""
         Base.__nb_objects = 0
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries."""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string."""
         if json_string is None or json_string == "":
