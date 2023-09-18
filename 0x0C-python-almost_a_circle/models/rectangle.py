f3ab3577174cd21b75c1c0a9fd160106d6a77d1f
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-18 20:14:03 +0300

    trying cases

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" rectangle file """
+""" Rectangle class """
 
 from models.base import Base
 
-
 class Rectangle(Base):
-    """rectangle class"""
+    """Rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """init of the class"""
+        """Initialize the Rectangle object"""
         super().__init__(id)
         self.int_validate("width", width)
         self.__width = width
@@ -21,62 +20,62 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """width"""
+        """Get the width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, val):
-        """set width"""
+        """Set the width of the rectangle"""
         self.int_validate("width", val)
         self.__width = val
 
     @property
     def height(self):
-        """return height"""
+        """Get the height of the rectangle"""
         return self.__height
 
     @height.setter
-    def height(self, height):
-        """height setter"""
-        self.int_validate("height", height)
-        self.__height = height
+    def height(self, val):
+        """Set the height of the rectangle"""
+        self.int_validate("height", val)
+        self.__height = val
 
     @property
     def x(self):
-        """return x"""
+        """Get the x-coordinate of the rectangle"""
         return self.__x
 
     @x.setter
-    def x(self, x):
-        """x setter"""
-        self.position_validate("x", x)
-        self.__x = x
+    def x(self, val):
+        """Set the x-coordinate of the rectangle"""
+        self.position_validate("x", val)
+        self.__x = val
 
     @property
     def y(self):
-        """return y"""
+        """Get the y-coordinate of the rectangle"""
         return self.__y
 
     @y.setter
-    def y(self, y):
-        """y setter"""
-        self.position_validate("y", y)
-        self.__y = y
+    def y(self, val):
+        """Set the y-coordinate of the rectangle"""
+        self.position_validate("y", val)
+        self.__y = val
 
     def area(self):
-        """return area"""
+        """Calculate and return the area of the rectangle"""
         return self.__width * self.__height
 
     def display(self):
-        """print rectangle"""
-        for i in range(self.__y):
+        """Display the rectangle using '#' characters"""
+        for _ in range(self.__y):
             print()
-        for i in range(self.__height):
+        for _ in range(self.__height):
             print(" " * self.__x, end="")
             print("#" * self.__width)
 
     def update(self, *args, **kwargs):
-        """up the rectangle"""
+        """Update the attributes of the rectangle"""
         if args:
             if len(args) > 0:
                 self.id = args[0]
@@ -93,20 +92,20 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def __str__(self):
-        """return str"""
+        """Return a string representation of the rectangle"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def int_validate(self, name, val):
-        """validates if the value is an int"""
-        if type(val) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if val <= 0:
-            raise ValueError("{} must be > 0".format(name))
+    def int_validate(self, property_name, value):
+        """Validate that a property is an integer and > 0"""
+        if not isinstance(value, int):
+            raise TypeError(f"{property_name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{property_name} must be > 0")
 
-    def position_validate(self, name, val):
-        """validates if the value is an int"""
-        if type(val) is not int:
-            raise TypeError("{} must be a int".format(name))
-        if val < 0:
-            raise ValueError("{} must be >= 0".format(name))
+    def position_validate(self, property_name, value):
+        """Validate that a position property is an integer and >= 0"""
+        if not isinstance(value, int):
+            raise TypeError(f"{property_name} must be an integer")
+        if value < 0:
+            raise ValueError(f"{property_name} must be >= 0")
