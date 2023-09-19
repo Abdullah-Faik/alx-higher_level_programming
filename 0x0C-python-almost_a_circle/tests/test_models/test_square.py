eff2949facdb09b6bc04e2997cc5dc7c9879fe9c
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-20 01:56:58 +0300

    tasks

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -104,3 +104,100 @@ class Square_0_Test(unittest.TestCase):
         input = None
         expected = "[]"
         self.assertEqual(Base.to_json_string(input), expected)
+
+    def test_create_Square(self):
+        """Checks the create method."""
+        self.sqaure = Square(1, 2)
+        self.assertEqual(self.sqaure.x, 2)
+
+    def test_create_Square(self):
+        """Checks the create method."""
+        self.sqaure = Square(1, 2, 3)
+        self.assertEqual(self.sqaure.y, 3)
+
+    def test_create_Square(self):
+        """Checks the create method."""
+        self.sqaure = Square(1, 2, 3, 4)
+        self.assertEqual(self.sqaure.id, 4)
+
+    def test_size_string(self):
+        """Checks the size attribute."""
+        with self.assertRaises(TypeError):
+            self.sqaure = Square("1")
+
+    def test_size_negative(self):
+        """Checks the size attribute."""
+        with self.assertRaises(ValueError):
+            self.sqaure = Square(-1)
+
+    def test_x_string(self):
+        """Checks the size attribute."""
+        with self.assertRaises(TypeError):
+            self.sqaure = Square(1, "1")
+
+    def test_x_negative(self):
+        """Checks the size attribute."""
+        with self.assertRaises(ValueError):
+            self.sqaure = Square(1, -1)
+
+    def test_y_string(self):
+        """Checks the size attribute."""
+        with self.assertRaises(TypeError):
+            self.sqaure = Square(1, 1, "1")
+
+    def test_y_negative(self):
+        """Checks the size attribute."""
+        with self.assertRaises(ValueError):
+            self.sqaure = Square(1, 1, -1)
+
+    def test_size_zero(self):
+        """Checks the size attribute."""
+        with self.assertRaises(ValueError):
+            self.sqaure = Square(0)
+
+    def test_create(self):
+        """Test create method."""
+        r1 = Square(3, 5, 1, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Square.create(**r1_dictionary)
+        self.assertEqual(r1.__str__(), r2.__str__())
+
+    def test_1_create(self):
+        """Test create method."""
+        r2 = Square.create(**{'id': 89})
+        self.assertEqual(r2.__str__(), "[Square] (89) 0/0 - 1")
+
+    def test_2_create(self):
+        """Test create method."""
+        r2 = Square.create(**{'id': 89, 'size': 1})
+        self.assertEqual(r2.__str__(), "[Square] (89) 0/0 - 1")
+
+    def test_3_create(self):
+        """Test create method."""
+        r2 = Square.create(**{'id': 89, 'size': 1, 'x': 2})
+        self.assertEqual(r2.__str__(), "[Square] (89) 2/0 - 1")
+
+    def test_4_create(self):
+        """Test create method."""
+        r2 = Square.create(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
+        self.assertEqual(r2.__str__(), "[Square] (89) 2/3 - 1")
+
+    def test_save_to_file(self):
+        """Test save_to_file method."""
+        r1 = Square(10, 7, 2, 8)
+        r2 = Square(2, 4, 0, 0)
+        Square.save_to_file([r1, r2])
+        with open("Square.json", "r") as f:
+            self.assertEqual(len(f.read()), 77)
+
+    def test_save_to_file_None(self):
+        """Test save_to_file method."""
+        Square.save_to_file(None)
+        with open("Square.json", "r") as f:
+            self.assertEqual(len(f.read()), 2)
+
+    def test_save_to_file_empty(self):
+        """Test save_to_file method."""
+        Square.save_to_file([])
+        with open("Square.json", "r") as f:
+            self.assertEqual(len(f.read()), 2)
