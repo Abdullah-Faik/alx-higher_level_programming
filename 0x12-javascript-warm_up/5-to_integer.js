7a3bd54f1c87bfb86241b72c39a3e8b95ae21369
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-12-12 02:51:03 +0200

    task num 6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const q = parseInt(process.argv[2]);
 if (isNaN(q)) {
-  console.log('Not a number');
+  console.log("Not a number");
 } else {
-  console.log('My number:',q);
+  console.log("My number:", q);
 }
