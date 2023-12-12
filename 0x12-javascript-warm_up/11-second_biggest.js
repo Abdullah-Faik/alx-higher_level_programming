9ab12d6f76111a179342585a7233e496b3ee940a
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-12-12 03:22:02 +0200

    task num 12

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+  const numbers = process.argv
+    .slice(2)
+    .map((value) => {
+      return +value;
+    })
+    .sort((a, b) => b - a);
+
+  console.log(numbers[1]);
+}
