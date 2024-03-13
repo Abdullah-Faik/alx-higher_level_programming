f1847accfd3da9b64fd9d84d4d9692d9c5572b50
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-03-13 21:02:34 +0200

    add last files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 --create user
-CREATE USER IF NOT EXIST 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_pwd';
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
