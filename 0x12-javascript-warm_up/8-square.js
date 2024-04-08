73021014d0000f7a57c9b6ac84ec350ba7e43bb4
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-08 22:45:20 +0200

    task 9

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,13 +2,12 @@
 const q = parseInt(process.argv[2]);
 
 if (isNaN(q)) {
-    console.log('Missing size');
-}
-else {
-    for (let i = 0; i < q; i++) {
-        for (let j = 0; j < q; j++) {
-            process.stdout.write('X')
-        }
-        process.stdout.write('\n');
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < q; i++) {
+    for (let j = 0; j < q; j++) {
+      process.stdout.write('X');
     }
+    process.stdout.write('\n');
+  }
 }
