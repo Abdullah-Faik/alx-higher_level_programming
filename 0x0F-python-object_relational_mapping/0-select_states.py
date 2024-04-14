43e16ed3ec7b508cf1fef96ac7c142d08882f045
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-14 09:51:31 +0200

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,11 +8,12 @@ import sys
 if __name__ == "__main__":
     """main function"""
     my_host = "localhost"
+    port = 3306
     my_user = sys.argv[1]
     my_password = sys.argv[2]
     d_base = sys.argv[3]
-    db = sql.connect(host=my_host, user=my_user, passwd=my_password, db=d_base)
-
+    db = sql.connect(host=my_host, port=port, user=my_user,
+                     passwd=my_password, db=d_base)
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
