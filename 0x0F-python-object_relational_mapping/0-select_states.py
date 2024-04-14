d05e1b021fcba021f279a867293d096de3940a34
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-14 09:57:08 +0200

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """lsiting a database"""
 
-import MySQLdb as sql
+import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     my_user = argv[1]
     my_password = argv[2]
     d_base = argv[3]
-    db = sql.connect(host=my_host, port=port, user=my_user,
+    db = MySQLdb.connect(host=my_host, port=port, user=my_user,
                      passwd=my_password, db=d_base)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
