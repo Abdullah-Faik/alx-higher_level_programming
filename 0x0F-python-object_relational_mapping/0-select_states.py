3fddb1874d0d9ca2574cd0a81753978d450161cd
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-14 10:01:01 +0200

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-"""lsiting a database"""
+"""
+List the contents of a database table.
+"""
 
 import MySQLdb as sql
 from sys import argv
 
-if __name__ == "__main__":
-    """main function"""
+
+def main():
+    """Main function."""
     my_host = "localhost"
     port = 3306
     my_user = argv[1]
     my_password = argv[2]
     d_base = argv[3]
+
     db = sql.connect(host=my_host, port=port, user=my_user,
                      passwd=my_password, db=d_base)
     cur = db.cursor()
+
     cur.execute("SELECT * FROM states ORDER BY states.id")
+
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
