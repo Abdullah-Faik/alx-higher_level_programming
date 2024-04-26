d53e2b2505a812aa8735f46e16b4fbb06ed72b6d
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-26 15:47:04 +0300

    1st task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""print type response """
+
+import urllib
+import urllib.request
+
+
+def main():
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
+        print(
+            'Body response:\n'
+            f'\t- type: {type(body)}\n'
+            f'\t- content: {body}\n'
+            f'\t- utf8 content: {body.decode("utf-8")}'
+        )
+
+if __name__ == "__main__":
+    main()
