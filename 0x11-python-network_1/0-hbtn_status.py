e8f9768dbd4f0f1ee202ef35562fb7902e719bb8
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-26 15:55:16 +0300

    1st task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,9 @@
 
 import urllib
 
+
 def main():
-    """print request data"""
+    """print request"""
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         body = res.read()
         print(
@@ -13,6 +14,7 @@ def main():
             f'\t- content: {body}\n'
             f'\t- utf8 content: {body.decode("utf-8")}'
         )
+
 
 if __name__ == "__main__":
     main()
